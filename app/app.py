6dbad27e17f4f964b07f207259180f4262ac33e6
Optimus-Q <pereira.melroy@outlook.com>
2025-12-17 17:30:29 +1030

    Adding application and testing files

---
+++ app/app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def add(a, b):
+    return a+b
+
+st.title("Simple Addition App")
+
+a = st.number_input("Enter the first number: ", 0, 100, key="First element")
+b = st.number_input("Enter the second number: ", 0, 100, key="Second element")
+
+if st.button("Add", key="Add button"):
+    result = add(a, b)
+    st.markdown(f"The result is: {result}")
